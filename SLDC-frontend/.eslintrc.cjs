863b81a1c916c7bd8298899340db9696d298f0b4
Zaid <predcodes695@gmail.com>
2024-05-20 22:02:44 +0530

    init: initialised repo, configured eslint, prettier, ts

---
+++ SLDC-frontend/.eslintrc.cjs
@@ -0,0 +1,63 @@
+module.exports = {
+    root: true,
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    extends: [
+      'airbnb',
+      'airbnb/hooks',
+      'airbnb-typescript',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react-hooks/recommended',
+      'plugin:prettier/recommended',  //adding it at last to override other configs
+    ],
+    ignorePatterns: ['.eslintrc.cjs'],
+    parserOptions: {
+      parser: '@typescript-eslint/parser',
+      project: './tsconfig.json',
+      tsconfigRootDir: __dirname,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    plugins: [
+      'react',
+      '@typescript-eslint',
+      'prettier'
+    ],
+    rules: {
+      'react/react-in-jsx-scope': 0,
+      'react/function-component-definition': [
+        2,
+        {
+          namedComponents: ['arrow-function', 'function-declaration'],
+          unnamedComponents: 'arrow-function',
+        },
+      ],
+      '@typescript-eslint/explicit-function-return-type': 'warn',
+      '@typescript-eslint/explicit-module-boundary-types': 'warn',
+      '@typescript-eslint/explicit-member-accessibility': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/typedef': [
+      'warn',
+      {
+        arrowParameter: true,
+        variableDeclaration: true,
+        memberVariableDeclaration: true,
+        objectDestructuring: true,
+        arrayDestructuring: true,
+        parameter: true,
+        propertyDeclaration: true,
+        variableDeclarationIgnoreFunction: true,
+      },
+    ],
+    },
+};
---
+++ SLDC-frontend/src/App.tsx
@@ -0,0 +1,8 @@
+const App = (): JSX.Element => {
+  return (
+    <div>
+      <h1>React App</h1>
+    </div>
+  );
+};
+export default App;
