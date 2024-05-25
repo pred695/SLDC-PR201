02ca44a3a7d766d986a448a5b8c81327cc078253
Shraman <pkpaulpostdn@gmail.com>
2024-05-25 14:44:33 +0530

    add: margins to the tables

--- SLDC-frontend/src/App.tsx
+++ SLDC-frontend/src/App.tsx
@@ -1,40 +1,38 @@
 import { Route, Routes } from 'react-router-dom';
-// import { Box, Heading, Center } from '@chakra-ui/react';
+import { Box, Heading, Center, Spacer } from '@chakra-ui/react';
+import { VStack } from "@chakra-ui/react";
+import AreaChart from 'react-apexcharts';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import Login from './pages/Login';
-import AreaChart from 'react-apexcharts';
 import OutlineGraph from './graphs/OutlineGraph';
-// import MyTableHourly from './components/tablesHourlyLoad';
-// import MyTableTotal from './components/tablesTotalLoad';
+import MyTableHourly from './components/tablesHourlyLoad';
+import MyTableTotal from './components/tablesTotalLoad';
 import MainGraph from './graphs/MainGraph';
 import { outlineData } from './tempData/outlineData';
 import { mainData, labels } from './tempData/mainData';
-// import MyTableHourlyAllzones from './components/tablesHourlyLoadAllzones';
+import MyTableHourlyAllzones from './components/tablesHourlyLoadAllzones';
 
 const App: React.FC = (): JSX.Element => {
   return (
     <>
       <Navbar />
       <Sidebar />
-      {/* <Box>
+      <Box>
         <Center>
-          <Heading>Project Setup Done!</Heading>
-          <MyTableHourly />
-          <MyTableTotal />
-          <MyTableHourlyAllzones />
+          {/* <Heading>Project Setup Done!</Heading> */}
+          <VStack spacing={15} marginTop={20}>
+            <MyTableHourly />
+            <MyTableTotal />
+            <MyTableHourlyAllzones />
+          </VStack>
         </Center>
-      </Box> */}
+      </Box>
       <Routes>
         <Route path="/signin" element={<Login />} />
         <Route
           path="/mainGraph"
-          element={
-            <MainGraph
-          data={mainData}
-          labels={labels}
-        />
-          }
+          element={<MainGraph data={mainData} labels={labels} />}
         />
         <Route
           path="/outlineGraph"
